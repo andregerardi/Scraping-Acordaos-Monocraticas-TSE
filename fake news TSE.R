2371a081c40f7b9067e4b4aa75815798e7bf1948
Dirceu André Gerardi <48441841+andregerardi@users.noreply.github.com>
2021-08-27 07:28:45 -0300

    Update fake news TSE.R

--- fake news TSE.R
+++ fake news TSE.R
@@ -8,14 +8,14 @@ nullToNA <- function(x) {
 }
 
 #fake news
-url_base <- "http://www.tse.jus.br/jurisprudencia/@@monocraticas-search?url=&q=&as_epq=fake+news&start=CONTADORLINK"
+url_base <- "http://www.tse.jus.br/jurisprudencia/@@monocraticas-search?url=&q=&as_epq=fake+news&start=CONT"
 
 #Raspando os textos
 TextoTSE <- data.frame()
 for (i in 1:7){
   print(i)
   i <- (i - 1) * 10 + 0
-  url <- gsub("CONTADORLINK", i, url_base)
+  url <- gsub("CONT", i, url_base)
   
   pagina <- readLines(url)
   pagina <- htmlParse(pagina, encoding = "UTF-8")
